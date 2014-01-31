8c814fd411c57d0979b375f60168aad182ecce37
Nesbert Hidalgo <nesbert@gmail.com>
2014-01-30 22:00:09 -0800

    updated to 3.0.9

--- adapters/standalone-framework.js
+++ adapters/standalone-framework.js
@@ -1,5 +1,5 @@
 /*
- Highcharts JS v3.0.8 (2014-01-09)
+ Highcharts JS v3.0.9 (2014-01-15)
 
  Standalone Highcharts Framework
 
--- modules/exporting.js
+++ modules/exporting.js
@@ -1,5 +1,5 @@
 /*
- Highcharts JS v3.0.8 (2014-01-09)
+ Highcharts JS v3.0.9 (2014-01-15)
  Exporting module
 
  (c) 2010-2014 Torstein Honsi
