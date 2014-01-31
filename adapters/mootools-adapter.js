d9ced6964c74611f79282e0f8a033c3a12ceba9a
Nesbert Hidalgo <nesbert@gmail.com>
2014-01-30 21:54:17 -0800

    updated to 3.0.8

--- adapters/mootools-adapter.js
+++ adapters/mootools-adapter.js
@@ -1,8 +1,8 @@
 /*
- Highcharts JS v3.0.7 (2013-10-24)
+ Highcharts JS v3.0.8 (2014-01-09)
  MooTools adapter
 
- (c) 2010-2013 Torstein Hønsi
+ (c) 2010-2014 Torstein Honsi
 
  License: www.highcharts.com/license
 */
